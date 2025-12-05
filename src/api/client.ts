62833bbaaf7403fbcaf186b89cf8a333afb6c4b5
Hugo DALLIAS <hugo.dallias@epita.fr>
2025-12-05 23:46:27 +0100

    fix(api/client.ts): fix backend route issues

--- src/api/client.ts
+++ src/api/client.ts
@@ -3,8 +3,8 @@ import axios, { AxiosHeaders } from "axios";
 // Si VITE_API_BASE_URL est défini, on l'utilise, sinon on reste sur /api
 const apiBaseUrl =
     (import.meta.env.VITE_API_BASE_URL
-        ? `${import.meta.env.VITE_API_BASE_URL}/api`
-        : "/api");
+        ? `${import.meta.env.VITE_API_BASE_URL}/`
+        : "/");
 
 const client = axios.create({
     baseURL: apiBaseUrl,
