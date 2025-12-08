b73641695667f56f4ebea339992d246a5831854a
Hugo DALLIAS <hugo.dallias@epita.fr>
2025-12-08 15:39:34 +0100

    fix(vite.config.js): fix config issues

--- vite.config.js
+++ vite.config.js
@@ -7,16 +7,18 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), "VITE_");
     const HOST = env.VITE_API_HOST || "api.example.com";
     const PORT = env.VITE_API_PORT || "443";
+    const URL = env.VITE_API_URL || "http://api.example.com:3000/api";
 
     console.info("API Host:", HOST);
     console.info("API Port:", PORT);
+    console.log("API Url:", URL);
 
     return {
         plugins: [react()],
         server: {
             proxy: {
                 "/api": {
-                    target: `http://${HOST}:${PORT}`,
+                    target: URL,
                     changeOrigin: true,
                     secure: false,
                     rewrite: (path) => path.replace(/^\/api/, "/smoke"),
